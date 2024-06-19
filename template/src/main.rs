930e077b39c377fa3b5ab35f08dd391e2ba6973c
j03-dev <24nomeniavo@gmail.com>
2024-06-19 17:17:40 +0300

    update the template for version 2.0

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,54 +1,16 @@
+use russenger::models::RussengerUser;
 use russenger::prelude::*;
 
-#[action]
-async fn Main (res: Res, req: Req) {
-    res.send(TextModel::new(&req.user, "Main, I'm your chatbot!"))
-        .await;
-
-    let payload_1 = Payload::new(Option1, Some(Data::new("payload_for_option_1", None)));
-    let payload_2 = Payload::new(Option2, Some(Data::new("payload_for_option_2", None)));
-
-    let replies = vec![
-        QuickReply::new("Option1", "", payload_1),
-        QuickReply::new("Option2", "", payload_2),
-    ];
-    res.send(QuickReplyModel::new(
-        &req.user,
-        "Choose an option:",
-        replies,
-    ))
-    .await;
-}
 
 #[action]
-async fn Option1(res: Res, req: Req){
-    let value: String = req.data.get_value();
-    let message = format!("You selected Option 1 with payload: {}", value);
-    res.send(TextModel::new(&req.user, &message)).await;
+async fn Main(res: Res, req: Req) {
+    res.send(TextModel::new(&req.user, "HelloWord")).await;
 }
 
-#[action]
-async fn Option2(res: Res, req: Req){
-    let value: String = req.data.get_value();
-    let message = format!("You selected Option 2 with payload: {}", value);
-    res.send(TextModel::new(&req.user, &message)).await;
-
-    let generic_elements = vec![GenericElement::new(
-        "Option 2",
-        "https://example.com/option2.jpg",
-        "Option 2 description",
-        vec![Button::Postback {
-            title: "Choose Option 2".to_string(),
-            payload: Payload::new(Main, None),
-        }],
-    )];
-
-    res.send(GenericModel::new(
-        &req.user,
-        generic_elements,
-        req.data.get_page(),
-    ))
-    .await;
+#[russenger::main]
+async fn main() {
+    let conn = Database::new().await.conn;
+    migrate!([RussengerUser], conn);
+    russenger::actions![Main];
+    russenger::launch().await;
 }
-
-russenger_app!(Main, Option1, Option2);
