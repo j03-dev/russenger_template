131eba0e7dd386352610826173b60404e1203f14
j03-dev <24nomeniavo@gmail.com>
2024-12-01 13:22:49 +0300

    update the template to 3-2rc

--- template/src/main.rs
+++ template/src/main.rs
@@ -4,13 +4,14 @@ use russenger::prelude::*;
 
 #[action]
 async fn Main(res: Res, req: Req) {
-    res.send(TextModel::new(&req.user, "HelloWord")).await;
+    res.send(TextModel::new(&req.user, "HelloWord")).await?;
+    Ok(())
 }
 
 #[russenger::main]
-async fn main() {
+async fn main() -> error::Result<()> {
     let conn = Database::new().await.conn;
     migrate!([RussengerUser], conn);
     russenger::actions![Main];
-    russenger::launch().await;
+    russenger::launch().await?;
 }
